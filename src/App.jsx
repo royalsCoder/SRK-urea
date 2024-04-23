adaf91dd0d14699c10db19c65eb838fc27ed6752
royal coders <rahul>
2024-04-24 00:08:12 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -291,12 +291,9 @@ function App() {
                     11, KASHIPUR DHANPALPUR, KHUSHIPUR SADAR,AH 1 BHADWAR <br />
                     VARANASI ,UTTAR PRADESH -226021
                   </p>
-                  <p className="m-0">
+                  <p className="mb-3">
                     Phone No : 9569853060 E-mail : defdke@gmail.com
                   </p>
-                  <h5 className="m-0 mainheading">GST-INVOICE</h5>
-
-                  <p className="m-0">Mobile No: 1234567890</p>
                 </div>
                 <div className="col-2"></div>
               </div>
@@ -349,56 +346,50 @@ function App() {
                   style={{ width: "6rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 >
+                  <strong>HSN/SAL</strong>
+                </div>
+                <div
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                >
+                  <strong>Unit</strong>
+                </div>
+                <div
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                >
                   <strong>Qyt.</strong>
                 </div>
                 <div
-                  style={{ width: "6rem" }}
+                  style={{ width: "5rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 >
-                  <strong>Unit.</strong>
+                  <strong>Rate</strong>
                 </div>
                 <div
                   style={{ width: "6rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 >
-                  <strong>Price</strong>
+                  <strong>Amount</strong>
+                </div>
+                <div
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                >
+                  <strong>Disc.</strong>
+                </div>
+                <div
+                  style={{ width: "7rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                >
+                  <strong>Taxable Amt</strong>
                 </div>
                 <div
                   style={{ width: "6rem" }}
                   className="col-3 p-0  ps-2 py-2 d-flex justify-content-start align-items-center"
                 >
-                  <strong>Totle</strong>
+                  <strong>Net Amt</strong>
                 </div>
-              </div>
-
-              <div className="row m-0 ">
-                <div
-                  style={{ width: "3rem" }}
-                  className="col-2 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
-                >
-                  1
-                </div>
-                <div className="col p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center">
-                  {formData?.itemName}
-                </div>
-                <div
-                  style={{ width: "6rem" }}
-                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
-                >
-                  {formData?.quantity}
-                </div>
-                <div
-                  style={{ width: "6rem" }}
-                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
-                ></div>
-                <div
-                  style={{ width: "6rem" }}
-                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
-                ></div>
-                <div
-                  style={{ width: "6rem" }}
-                  className="col-3 p-0  ps-2 py-2 d-flex justify-content-start align-items-center"
-                ></div>
               </div>
 
               <Table />
@@ -422,11 +413,27 @@ function App() {
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
-                  style={{ width: "6rem" }}
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                ></div>
+                <div
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                ></div>
+                <div
+                  style={{ width: "5rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
                   style={{ width: "6rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                ></div>
+                <div
+                  style={{ width: "4rem" }}
+                  className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+                ></div>
+                <div
+                  style={{ width: "7rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
@@ -448,11 +455,11 @@ function App() {
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
-                  style={{ width: "6rem" }}
+                  style={{ width: "4rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
-                  style={{ width: "6rem" }}
+                  style={{ width: "7rem" }}
                   className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
                 ></div>
                 <div
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Table = () => {
   return (
-    <div>
-      <div className="row m-0 ">
+    <>
+      <div className="row m-0  ">
         <div
           style={{ width: "3rem" }}
           className="col-2 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
@@ -14,11 +14,27 @@ const Table = () => {
           className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
         ></div>
         <div
-          style={{ width: "6rem" }}
+          style={{ width: "4rem" }}
+          className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+        ></div>
+        <div
+          style={{ width: "4rem" }}
+          className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+        ></div>
+        <div
+          style={{ width: "5rem" }}
           className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
         ></div>
         <div
           style={{ width: "6rem" }}
+          className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+        ></div>
+        <div
+          style={{ width: "4rem" }}
+          className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
+        ></div>
+        <div
+          style={{ width: "7rem" }}
           className="col-3 p-0 invoice-border-right ps-2 py-2 d-flex justify-content-start align-items-center"
         ></div>
         <div
@@ -26,7 +42,7 @@ const Table = () => {
           className="col-3 p-0  ps-2 py-2 d-flex justify-content-start align-items-center"
         ></div>
       </div>
-    </div>
+    </>
   );
 };
 
